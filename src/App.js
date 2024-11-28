7eaf311d89aa15fb4a7d19d16e5a374994c460b5
darshan2436 <thapadarshan447@gmail.com>
2024-11-28 09:07:20 +0545

    initialized file we will be working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home.js";
+import Appointment from "./Appointment.js";
+import Donation from "./Donation.js";
+import Login from "./Login.js";
+import Register from "./Register.js";
+
 function App() {
   return (
     <div className="">
-      <h1 className='text-3xl'>Dirghaayu</h1>
+      <Router>
+        <nav className="m-5 ">
+          <Link to="/" className="mx-4 hover:text-blue-500 text-xl ">Home</Link>
+          <Link to="/appointment" className="mx-4 hover:text-blue-500 text-xl ">Appointment</Link>
+          <Link to="/donation" className="mx-4 hover:text-blue-500 text-xl ">Donation</Link>
+          <Link to="/login" className="mx-4 hover:text-blue-500 text-xl ">Login</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/donation" element={<Donation />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Donation.js
@@ -0,0 +1,9 @@
+function Donation(){
+    return(
+        <div>
+            <h1>This is Donation section</h1>
+        </div>
+    )
+}
+
+export default Donation
