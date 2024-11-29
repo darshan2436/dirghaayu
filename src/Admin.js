4e19117214788087910ad616db640defc3a9cba2
darshan2436 <thapadarshan447@gmail.com>
2024-11-29 23:12:18 +0545

    Added the request data to be loaded in the admin page

--- src/Admin.js
+++ src/Admin.js
@@ -3,55 +3,154 @@ import axios from "axios";
 
 function Admin() {
   const [appointments, setAppointments] = useState([]);
+  const [donors, setDonors] = useState([]);
+  const [requests, setRequests] = useState([]);
 
   useEffect(() => {
-    // Fetch the list of appointments from the backend
+    // Fetch Appointments
     axios
-      .get("http://localhost:4000/api/appointment/showappointment")
+      .get("http://localhost:4000/api/appointment/showappointment") // Adjust endpoint as needed
       .then((response) => {
-        setAppointments(response.data); // Store the appointment data
+        setAppointments(response.data);
       })
       .catch((error) => {
         console.error("Error fetching appointments:", error);
+      });
+
+    // Fetch Donors
+    axios
+      .get("http://localhost:4000/api/donor/show") // Adjust endpoint as needed
+      .then((response) => {
+        setDonors(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching donors:", error);
+      });
+
+    // Fetch Requests
+    axios
+      .get("http://localhost:4000/api/request/show") // Adjust endpoint as needed
+      .then((response) => {
+        setRequests(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching requests:", error);
       });
   }, []);
 
   return (
     <div className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 min-h-screen py-10">
       <div className="container mx-auto bg-slate-200 opacity-90 p-6 rounded-lg shadow-lg">
-        <h2 className="text-3xl font-bold mb-6 text-center">Appointments Booked</h2>
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-white shadow-md rounded-lg">
-            <thead className="bg-gray-800 text-white">
-              <tr>
-                <th className="py-2 px-4">Name</th>
-                <th className="py-2 px-4">Hospital</th>
-                <th className="py-2 px-4">Doctor</th>
-                <th className="py-2 px-4">Date</th>
-                <th className="py-2 px-4">Time</th>
-                <th className="py-2 px-4">Notes</th>
-              </tr>
-            </thead>
-            <tbody>
-              {appointments.length > 0 ? (
-                appointments.map((appointment, index) => (
-                  <tr key={index} className="text-center">
-                    <td className="py-2 px-4 border">{appointment.fullName}</td>
-                    <td className="py-2 px-4 border">{appointment.hospital}</td>
-                    <td className="py-2 px-4 border">{appointment.doctor}</td>
-                    <td className="py-2 px-4 border">{appointment.date}</td>
-                    <td className="py-2 px-4 border">{appointment.time}</td>
-                    <td className="py-2 px-4 border">{appointment.reason}</td>
-                  </tr>
-                ))
-              ) : (
+        
+        {/* Appointments Section */}
+        <div className="mb-10">
+          <h2 className="text-3xl font-bold mb-6 text-center">Appointments Booked</h2>
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-white shadow-md rounded-lg">
+              <thead className="bg-gray-800 text-white">
                 <tr>
-                  <td colSpan="6" className="py-2 px-4 text-center">No appointments booked</td>
+                  <th className="py-2 px-4">Name</th>
+                  <th className="py-2 px-4">Hospital</th>
+                  <th className="py-2 px-4">Doctor</th>
+                  <th className="py-2 px-4">Date</th>
+                  <th className="py-2 px-4">Time</th>
+                  <th className="py-2 px-4">Notes</th>
                 </tr>
-              )}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {appointments.length > 0 ? (
+                  appointments.map((appointment, index) => (
+                    <tr key={index} className="text-center">
+                      <td className="py-2 px-4 border">{appointment.fullName}</td>
+                      <td className="py-2 px-4 border">{appointment.hospital}</td>
+                      <td className="py-2 px-4 border">{appointment.doctor}</td>
+                      <td className="py-2 px-4 border">{appointment.date}</td>
+                      <td className="py-2 px-4 border">{appointment.time}</td>
+                      <td className="py-2 px-4 border">{appointment.reason}</td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="6" className="py-2 px-4 text-center">No appointments booked</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
+
+        {/* Donor Section */}
+        <div className="mb-10">
+          <h2 className="text-3xl font-bold mb-6 text-center">Donors</h2>
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-white shadow-md rounded-lg">
+              <thead className="bg-gray-800 text-white">
+                <tr>
+                  <th className="py-2 px-4">Name</th>
+                  <th className="py-2 px-4">Blood Group</th>
+                  <th className="py-2 px-4">Location</th>
+                  <th className="py-2 px-4">Phone</th>
+                </tr>
+              </thead>
+              <tbody>
+                {donors.length > 0 ? (
+                  donors.map((donor, index) => (
+                    <tr key={index} className="text-center">
+                      <td className="py-2 px-4 border">{donor.userName}</td>
+                      <td className="py-2 px-4 border">{donor.bloodGroup}</td>
+                      <td className="py-2 px-4 border">{donor.location}</td>
+                      <td className="py-2 px-4 border">{donor.phone}</td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="4" className="py-2 px-4 text-center">No donors found</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        {/* Requests Section */}
+        <div>
+          <h2 className="text-3xl font-bold mb-6 text-center">Requests</h2>
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-white shadow-md rounded-lg">
+              <thead className="bg-gray-800 text-white">
+                <tr>
+                  <th className="py-2 px-4">Requester Name</th>
+                  <th className="py-2 px-4">Blood Group</th>
+                  <th className="py-2 px-4">Contact</th>
+                  <th className="py-2 px-4">Email</th>
+                  <th className="py-2 px-4">Address</th>
+                  <th className="py-2 px-4">Age</th>
+                  <th className="py-2 px-4">Gender</th>
+                </tr>
+              </thead>
+              <tbody>
+                {requests.length > 0 ? (
+                  requests.map((request, index) => (
+                    <tr key={index} className="text-center">
+                      <td className="py-2 px-4 border">{request.userName}</td>
+                      <td className="py-2 px-4 border">{request.bloodGroup}</td>
+                      <td className="py-2 px-4 border">{request.contact}</td>
+                      <td className="py-2 px-4 border">{request.email}</td>
+                      <td className="py-2 px-4 border">{request.address}</td>
+                      <td className="py-2 px-4 border">{request.age}</td>
+                      <td className="py-2 px-4 border">{request.gender}</td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="4" className="py-2 px-4 text-center">No requests found</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+
       </div>
     </div>
   );
--- src/RequestForm.jsx
+++ src/RequestForm.jsx
@@ -1,51 +1,183 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 const RequestForm = () => {
+  const [formData, setFormData] = useState({
+    userName: "",
+    bloodType: "",
+    contact: "",
+    email: "",
+    address: "",
+    age: "",
+    gender: "",
+  });
+
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/api/request/register",
+        formData
+      );
+      setMessage("Request successfully submitted!");
+      setFormData({
+        userNameame: "",
+        bloodType: "",
+        contact: "",
+        email: "",
+        address: "",
+        age: "",
+        gender: "",
+      });
+    } catch (error) {
+      console.error("Error submitting request:", error);
+      setMessage("Failed to submit the request. Please try again.");
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-r from-green-100 via-blue-100 to-purple-100 flex items-center justify-center p-4">
       <div className="rounded-md border p-6 bg-gradient-to-b from-white to-gray-100 shadow-md w-full max-w-2xl mx-auto">
-        <form className="p-6 rounded border border-gray-300 shadow-md bg-gradient-to-r from-green-200 via-blue-400 to-purple-200">
+        <form
+          onSubmit={handleSubmit}
+          className="p-6 rounded border border-gray-300 shadow-md bg-gradient-to-r from-green-200 via-blue-400 to-purple-200"
+        >
           <fieldset className="bg-white p-6 rounded-md">
             <h1 className="text-3xl mb-4 text-gray-800">Request Form</h1>
+            {message && (
+              <p
+                className={`mb-4 ${
+                  message.includes("successfully")
+                    ? "text-green-600"
+                    : "text-red-600"
+                }`}
+              >
+                {message}
+              </p>
+            )}
             <div className="mb-4">
-              <label className="block text-gray-800 font-semibold" htmlFor='name'>Name</label>
-              <input type="text" id="name" placeholder="Name" className="block w-full p-3 border rounded-md"/>
+              <label className="block text-gray-800 font-semibold" htmlFor="name">
+                Name
+              </label>
+              <input
+                type="text"
+                id="userName"
+                placeholder="Name"
+                value={formData.name}
+                onChange={handleChange}
+                className="block w-full p-3 border rounded-md"
+              />
             </div>
             <div className="mb-4">
-              <label className="block text-gray-800 font-semibold" htmlFor='bloodType'>Required Blood Type</label>
-              <input type="text" id="bloodType" placeholder="Required Blood Type" className="block w-full p-3 border rounded-md"/>
+              <label
+                className="block text-gray-800 font-semibold"
+                htmlFor="bloodType"
+              >
+                Required Blood Type
+              </label>
+              <input
+                type="text"
+                id="bloodType"
+                placeholder="Required Blood Type"
+                value={formData.bloodType}
+                onChange={handleChange}
+                className="block w-full p-3 border rounded-md"
+              />
             </div>
             <div className="mb-4">
-              <label className="block text-gray-800 font-semibold" htmlFor='contact'>Contact</label>
-              <input type="text" id="contact" placeholder="Contact" className="block w-full p-3 border rounded-md"/>
+              <label
+                className="block text-gray-800 font-semibold"
+                htmlFor="contact"
+              >
+                Contact
+              </label>
+              <input
+                type="text"
+                id="contact"
+                placeholder="Contact"
+                value={formData.contact}
+                onChange={handleChange}
+                className="block w-full p-3 border rounded-md"
+              />
             </div>
             <div className="mb-4">
-              <label className="block text-gray-800 font-semibold" htmlFor='email'>Email</label>
-              <input type="email" id="email" placeholder="Email" className="block w-full p-3 border rounded-md"/>
+              <label className="block text-gray-800 font-semibold" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                className="block w-full p-3 border rounded-md"
+              />
             </div>
             <div className="mb-4">
-              <label className="block text-gray-800 font-semibold" htmlFor='address'>Address</label>
-              <input type="text" id="address" placeholder="Address" className="block w-full p-3 border rounded-md"/>
+              <label
+                className="block text-gray-800 font-semibold"
+                htmlFor="address"
+              >
+                Address
+              </label>
+              <input
+                type="text"
+                id="address"
+                placeholder="Address"
+                value={formData.address}
+                onChange={handleChange}
+                className="block w-full p-3 border rounded-md"
+              />
             </div>
             <div className="mb-4">
-              <label className="block text-gray-800 font-semibold" htmlFor='age'>Age</label>
-              <input type="number" id="age" placeholder="Age" className="block w-full p-3 border rounded-md"/>
+              <label className="block text-gray-800 font-semibold" htmlFor="age">
+                Age
+              </label>
+              <input
+                type="number"
+                id="age"
+                placeholder="Age"
+                value={formData.age}
+                onChange={handleChange}
+                className="block w-full p-3 border rounded-md"
+              />
             </div>
             <div className="mb-4">
-              <label className="block text-gray-800 font-semibold" htmlFor='gender'>Gender</label>
-              <select id="gender" className="block w-full p-3 border rounded-md">
+              <label
+                className="block text-gray-800 font-semibold"
+                htmlFor="gender"
+              >
+                Gender
+              </label>
+              <select
+                id="gender"
+                value={formData.gender}
+                onChange={handleChange}
+                className="block w-full p-3 border rounded-md"
+              >
                 <option value="">Select</option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
               </select>
             </div>
-            <button type="submit" className="bg-green-500 text-white py-3 px-6 rounded">Submit</button>
+            <button
+              type="submit"
+              className="bg-green-500 text-white py-3 px-6 rounded"
+            >
+              Submit
+            </button>
           </fieldset>
         </form>
       </div>
     </div>
   );
-}
+};
 
 export default RequestForm;
